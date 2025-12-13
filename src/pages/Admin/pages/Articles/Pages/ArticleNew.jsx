c697cf8cd2f9867830d4b05daca047ae3539fb6a
Thamidu-Nadun <nadunrz101@gmail.com>
2025-12-13 23:58:58 +0530

    Add description field to article form, implement saveArticle function, and enhance error handling

--- src/pages/Admin/pages/Articles/Pages/ArticleNew.jsx
+++ src/pages/Admin/pages/Articles/Pages/ArticleNew.jsx
@@ -1,38 +1,67 @@
-import React, { useState } from "react";
-import BreadCrumb from "../../../../../Components/BreadCrumb/BreadCrumb";
-import { Calendar, Heading, Image, User } from "lucide-react";
-import Preview from "./Components/Preview";
+import React, {useRef, useState} from 'react';
+import BreadCrumb from '../../../../../Components/BreadCrumb/BreadCrumb';
+import {Calendar, Heading, Image, User, Rows2, Tags} from 'lucide-react';
+import Preview from './Components/Preview';
+import {saveArticle} from '../util';
+import toast from 'react-hot-toast';
 
 const ArticleNew = () => {
-  const [formContent, setFormContent] = useState({
-    title: "",
-    date: "",
-    author: "Nadun",
-    image: "",
-    content: "",
+  const [formContent, setFormContent] = useState ({
+    title: '',
+    date: '',
+    description: '',
+    author: 'Nadun',
+    image: '',
+    content: '',
+    tags: '',
   });
 
-  const [isPreviewMode, setIsPreviewMode] = useState(false);
+  const [isPreviewMode, setIsPreviewMode] = useState (false);
+  const [Publish, setPublish] = useState (false);
+  const categoryRef = useRef (null);
 
   const handleChange = (field, value) => {
-    setFormContent((prev) => ({
+    setFormContent (prev => ({
       ...prev,
-      [field.toLowerCase()]: value, // assuming field matches formContent keys
+      [field.toLowerCase ()]: value, // assuming field matches formContent keys
     }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted Data:", formContent);
+  const handleSubmit = e => {
+    e.preventDefault ();
+    console.log ('Submitted Data:', formContent);
+    console.log ('Publish Status:', Publish);
+    console.log ('Selected Category ID:', categoryRef.current.value);
+    saveArticle (
+      formContent,
+      Publish === true,
+      Number (categoryRef.current.value)
+    )
+      .then (data => {
+        toast.success ('Article saved successfully!');
+        setFormContent ({
+          title: '',
+          date: '',
+          description: '',
+          author: 'Nadun',
+          image: '',
+          content: '',
+          tags: '',
+        });
+      })
+      .catch (error => {
+        toast.error ('Error saving article.');
+        console.error ('Error:', error);
+      });
   };
 
   return (
     <div className="w-full p-4">
       <BreadCrumb
-        prev_name={"DashBoard"}
-        prev_link={"/dashboard"}
-        next_name={"Articles"}
-        next_link={"/dashboard/articles"}
+        prev_name={'DashBoard'}
+        prev_link={'/dashboard'}
+        next_name={'Articles'}
+        next_link={'/dashboard/articles'}
       />
       <div className="mt-15 ml-4 px-5">
         <h2 className="text-2xl">Create New Article</h2>
@@ -43,6 +72,13 @@ const ArticleNew = () => {
               input_type="text"
               Icon={Heading}
               value={formContent.title}
+              onChange={handleChange}
+            />
+            <InputGroup
+              input_name="Description"
+              input_type="text"
+              Icon={Rows2}
+              value={formContent.description}
               onChange={handleChange}
             />
             <InputGroup
@@ -72,38 +108,79 @@ const ArticleNew = () => {
             <div className="btn-group my-4 w-full flex justify-end md:justify-start">
               <button
                 type="button"
-                className={`${!isPreviewMode ? "bg-orange-400" : "bg-amber-400/20"} border border-orange-400 px-2 py-1 rounded mx-2`}
-                onClick={() => setIsPreviewMode(false)}
+                className={`${!isPreviewMode ? 'bg-orange-400' : 'bg-amber-400/20'} border border-orange-400 px-2 py-1 rounded mx-2`}
+                onClick={() => setIsPreviewMode (false)}
               >
                 Edit
               </button>
               <button
                 type="button"
-                className={`${isPreviewMode ? "bg-orange-400" : "bg-orange-400/20"} border border-orange-400 px-2 py-1 rounded mx-2`}
-                onClick={() => setIsPreviewMode(true)}
+                className={`${isPreviewMode ? 'bg-orange-400' : 'bg-orange-400/20'} border border-orange-400 px-2 py-1 rounded mx-2`}
+                onClick={() => setIsPreviewMode (true)}
               >
                 Preview
               </button>
             </div>
 
-            {!isPreviewMode ? (
-              <div className="editor-edit h-full">
-                <textarea
-                  value={formContent.content}
-                  onChange={(e) =>
-                    setFormContent((prev) => ({
-                      ...prev,
-                      content: e.target.value,
-                    }))
-                  }
-                  name="editor"
-                  id="editor"
-                  className="w-full h-full resize-none p-2 bg-gray-700/20 outline-none border border-orange-400"
-                />
-              </div>
-            ) : (
-              <Preview content={formContent.content} />
-            )}
+            {!isPreviewMode
+              ? <div className="editor-edit h-full">
+                  <textarea
+                    value={formContent.content}
+                    onChange={e =>
+                      setFormContent (prev => ({
+                        ...prev,
+                        content: e.target.value,
+                      }))}
+                    name="editor"
+                    id="editor"
+                    className="w-full h-full resize-none p-2 bg-gray-700/20 outline-none border border-orange-400"
+                  />
+                </div>
+              : <Preview content={formContent.content} />}
+          </div>
+
+          <div className="mt-20">
+            <select
+              name="isPublished"
+              defaultValue={Publish.toString ()}
+              onChange={e => setPublish (e.target.value === 'true')}
+              className="mt-4 p-2 border border-amber-500/10 rounded bg-amber-500/5"
+            >
+              <option value="false">Save as Draft</option>
+              <option value="true">Publish Now</option>
+            </select>
+          </div>
+
+          <div className="mt-4">
+            <select
+              name="category"
+              ref={categoryRef}
+              className="mt-4 p-2 border border-amber-500/10 rounded bg-amber-500/5"
+              defaultValue={''}
+            >
+              <option value="" disabled>Select Category</option>
+              <option value="1">Technology</option>
+              <option value="2">Health</option>
+              <option value="3">Lifestyle</option>
+              <option value="4">Education</option>
+              <option value="5">Entertainment</option>
+              <option value="6">Business</option>
+              <option value="7">Travel</option>
+              <option value="8">Food</option>
+              <option value="9">Sports</option>
+              <option value="10">Science</option>
+            </select>
+          </div>
+
+          <div>
+            <h4 className="mt-10 mb-2">Tags</h4>
+            <InputGroup
+              input_name="Tags"
+              input_type="text"
+              Icon={Image}
+              value={formContent.tags}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="btn-group mt-25 flex gap-2 w-full justify-end md:justify-start">
@@ -117,14 +194,13 @@ const ArticleNew = () => {
               type="button"
               className="px-3 py-2 bg-red-500 hover:bg-red-500/80 text-white rounded"
               onClick={() =>
-                setFormContent({
-                  title: "",
-                  date: "",
-                  author: "Nadun",
-                  image: "",
-                  content: "",
-                })
-              }
+                setFormContent ({
+                  title: '',
+                  date: '',
+                  author: 'Nadun',
+                  image: '',
+                  content: '',
+                })}
             >
               Discard
             </button>
@@ -138,10 +214,10 @@ const ArticleNew = () => {
 export default ArticleNew;
 
 const InputGroup = ({
-  input_name = "title",
-  input_type = "text",
+  input_name = 'title',
+  input_type = 'text',
   Icon = Heading,
-  value = "",
+  value = '',
   onChange,
 }) => (
   <div className="flex items-center text-orange-300 my-2 border bg-indigo-500/5 border-gray-500/10 rounded gap-1 pl-2">
@@ -150,9 +226,9 @@ const InputGroup = ({
       className="w-full outline-none bg-transparent py-2.5 pl-2 border border-transparent focus:border-indigo-400 invalid:border-red-400 valid:border-green-400"
       type={input_type}
       placeholder={input_name}
-      name={input_name.toLowerCase()}
+      name={input_name.toLowerCase ()}
       value={value}
-      onChange={(e) => onChange(input_name, e.target.value)}
+      onChange={e => onChange (input_name, e.target.value)}
       required
     />
   </div>
--- src/pages/Admin/pages/Articles/util.js
+++ src/pages/Admin/pages/Articles/util.js
@@ -1,55 +1,85 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 const getArticles = async () => {
-  try {
-    let res = await fetch(`${BASE_URL}articles/`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    let data = await res.json();
-    // console.log("data", data);
-    return data;
-  } catch (error) {
-    console.error("Error fetching articles:", error);
-    return [];
-  }
+    try {
+        let res = await fetch(`${BASE_URL}articles/`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let data = await res.json();
+        // console.log("data", data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching articles:", error);
+        return [];
+    }
 };
 
 const getArticleByCategoryId = async (categoryId) => {
-  try {
-    let res = await fetch(`${BASE_URL}articles/category?catId=${categoryId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    let data = await res.json();
-    return data;
-  } catch (error) {
-    console.error(
-      `Error fetching articles for category id ${categoryId}:`,
-      error,
-    );
-    return [];
-  }
+    try {
+        let res = await fetch(`${BASE_URL}articles/category?catId=${categoryId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(
+            `Error fetching articles for category id ${categoryId}:`,
+            error,
+        );
+        return [];
+    }
+};
+
+const saveArticle = async (article, isPublished, categoryId) => {
+    let payload = {
+        "title": article.title,
+        "description": article.description,
+        "coverImageUrl": article.image,
+        "published": isPublished,
+        "categoryId": categoryId || 1,
+        "tags": article.tags,
+        "body": article.content,
+        "authorId": article.userId || 1,
+        "publishedAt": article.date,
+    }
+
+    console.log("Payload to be sent:", payload);
+    try {
+        let res = await fetch(`${BASE_URL}articles/save`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        });
+        let data = await res.json();
+        return data;
+    } catch (error) {
+        console.error("Error saving article:", error);
+        return null;
+    }
 };
 
 const deleteArticleById = async (articleId) => {
-  try {
-    let res = await fetch(`${BASE_URL}articles/delete/${articleId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    let data = await res.json();
-    return data;
-  } catch (error) {
-    console.error(`Error deleting article with id ${articleId}:`, error);
-    return null;
-  }
+    try {
+        let res = await fetch(`${BASE_URL}articles/delete/${articleId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(`Error deleting article with id ${articleId}:`, error);
+        return null;
+    }
 };
 
-export { getArticles, getArticleByCategoryId, deleteArticleById };
+export { getArticles, saveArticle, getArticleByCategoryId, deleteArticleById };
